7909a11bebc1ba97ed4ff033ea009cc8dc6e86cc
darpnpandya <darpanpandya.dynamic.methods@gmail.com>
2025-03-09 11:59:56 +0530

    initial commit

---
+++ wp-content/plugins/event-tickets/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'the-events-calendar/event-tickets',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '8c6a976eee911f6cb16af25a3f20ca9538929969',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'the-events-calendar/event-tickets' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '8c6a976eee911f6cb16af25a3f20ca9538929969',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
---
+++ wp-content/themes/first-code/README.md
@@ -0,0 +1,2 @@
+This Is My First Project Upload On Git Hub Account So Please Try To Understand <br>
+Author Name : Darpan Pandya 
